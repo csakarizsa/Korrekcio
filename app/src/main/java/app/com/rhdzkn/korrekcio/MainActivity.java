926a6bdaf04e8759e46f1ec14406d9828df2c9ca
Richard Z <csakarizsa@gmail.com>
2015-09-26 21:08:18 +0200

    Kilépés létrehozása.

--- app/src/main/java/app/com/rhdzkn/korrekcio/MainActivity.java
+++ app/src/main/java/app/com/rhdzkn/korrekcio/MainActivity.java
@@ -1,8 +1,10 @@
 package app.com.rhdzkn.korrekcio;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
@@ -16,22 +18,25 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-   /* @Override
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_quit) {
+            killApplication();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
-    }*/
+    }
+
+    public void killApplication()
+    {
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
 }
